77c93a9081d6bdda8f7ae4c87dd6cf7e898638c6
Pawel Piwosz <pawel@pawelpiwosz.net>
2019-05-24 23:51:35 +0200

    fixes

--- tests/transformations.sh
+++ tests/transformations.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 actions=(
-    "convert plant.jpg -resize 50% resized50.jpg"
+    "convert plant.jpg -resize 50 resized50.jpg"
     "convert plant.jpg plant.png"
     "convert plant.jpg -blur 40x40 blur.jpg"
     "convert plant.jpg -rotate 90 rotate.jpg"
@@ -10,7 +10,7 @@ actions=(
 bucketpath="$(pwd)/tests/bucket"
 for action in "${actions[@]}"; do
     echo "${action}"
-    if ! docker run --rm -v "${bucketpath}":/bucket imagemagick "${action}"
+    if ! docker run --rm -v "${bucketpath}":/bucket imagemagick ${action}
     then
         echo "${action} failed!"
         exit 1

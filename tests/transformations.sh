3f2c3e5714e21b18e4982187e2a4461e28614169
Pawel Piwosz <pawel@pawelpiwosz.net>
2019-05-24 23:09:18 +0200

    travis

--- tests/transformations.sh
+++ tests/transformations.sh
@@ -3,7 +3,6 @@
 actions=(
   "convert plant.jpg -resize 50% resized50.jpg"
   "convert plant.jpg plant.png"
-  "convert dupa"
   "convert plant.jpg -blur 40x40 blur.jpg"
   "convert plant.jpg -rotate 90 rotate.jpg"
   "convert plant.jpg -negate negate.jpg"
